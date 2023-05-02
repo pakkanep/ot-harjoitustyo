2823134af221a18f1c3db4dc5423b387239f5895
pakkanep <patrikpakkanen9@gmail.com>
2023-05-02 20:28:22 +0300

    unittest

--- src/tests/infoseeker_test.py
+++ src/tests/infoseeker_test.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, MagicMock
 from services.infoseeker import InfoSeeker
 
 
@@ -40,4 +40,66 @@ class TestInfoSeeker(unittest.TestCase):
             "Node.js": 1,
             ".NET": 1
         })
+
+
+
+    def test_search_instances(self):
+        text = "We are looking for a Java developer who knows Java programming language and is familiar with Java frameworks like Spring and Hibernate."
+        expected_result = {
+            "Java": 2,
+            "Python": 1,
+            "C++": 1,
+            "Go": 1,
+            "Rust": 1,
+            "Javascript": 1,
+            "PHP": 1,
+            "C#": 1,
+            "Ruby": 1,
+            "C": 1,
+            "SQL": 1,
+            "PostgreSQL": 1,
+            "TypeScript": 1,
+            "Kotlin": 1,
+            "Scala": 1,
+            "React": 1,
+            "Node.js": 1,
+            ".NET": 1
+        }
+        self.seeker.search_instances(text)
+        result = self.seeker.information_dict
+        self.assertEqual(result, expected_result)
+
+
+    @patch('requests.get')
+    def test_search_links(self, mock_get):
+        url = 'https://duunitori.fi'
+        mock_response = MagicMock()
+        mock_response.text = """
+        <html><body><a class="job-box__hover gtm-search-result" 
+        href="/job/123">Job 1</a><a class="job-box__hover gtm-search-result" 
+        href="/job/456">Job 2</a></body></html>
+        """
+        mock_get.return_value = mock_response
+        self.seeker.handle = MagicMock()
+        self.seeker.search_links(url)
+        self.seeker.handle.assert_any_call('https://duunitori.fi/job/123')
+        self.seeker.handle.assert_any_call('https://duunitori.fi/job/456')
+
+
+"""
+    def test_search_amount_of_ads(self):
+        pass
+
+    def test_search_amount_of_pages(self):
+        pass
+
+    def test_seek_all_pages(self):
+        pass
+
+    def test_handle(self):
+        pass
+
+    def test_start(self):
+        pass
+"""
 
